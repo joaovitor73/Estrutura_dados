ce7a6d7358738af5f6af8814b2043ad460b38154
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2024-02-29 15:36:50 -0300

    Add pilha

---
+++ Pilha/Pilha.js
@@ -0,0 +1,84 @@
+class Pilha{
+
+    /** 
+      * creat a pilha
+      * @constructor
+      * @param {tam} tam - O tamanho da lista
+    */
+    constructor(tam = 5){
+        this.dados = [];
+        this.topo = 0;
+        this.tam = tam;
+    }
+
+    /**
+     * Create the method push
+     * @push
+     * @param {dado} dado - Dado a ser inserido na pilha
+     */
+
+    push(dado){
+        if(!this.isFull()){
+            this.dados[this.topo++] = dado;
+            return;
+        }
+        throw new Error("Stackoverflow");
+    }
+
+
+    /**
+     * Create the method pop
+     * @pop
+     */
+    pop(){
+        if(!this.isEmpty()){
+            this.topo--;
+            return;
+        }
+        throw new Error("Stackunderflow");
+    }
+
+    /**
+     * Create the topo
+     * @topo
+     */
+
+    top(){
+        return this.dados[this.topo-1];
+    }
+
+    /**
+     * Create the method clear 
+     * @clear
+     */
+    clear(){
+        this.topo = 0;
+    }
+
+    /**
+     * Crate the method size
+     * @size
+     */
+    size(){
+       return this.topo;
+    }
+
+    /**
+     * Create the method isEmpty
+     * @isEmpty
+     */
+    isEmpty(){
+        return this.size() === 0;
+    }
+
+    /**
+     * Create the method isFull
+     * @isFull
+     */
+
+    isFull(){
+        return this.size() === this.tam;
+    }
+}
+
+export default Pilha;
