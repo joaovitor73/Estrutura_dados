e36aee16d5fb0881a8a4a58507961cfdca9380fb
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2024-03-08 18:20:24 -0300

    Add dec to bin with stack

---
+++ lista_01/decToBin.test.js
@@ -0,0 +1,8 @@
+import decToBin from "./decToBin";
+
+test("Conversor de dec para bin",
+    () => {
+        expect(decToBin(50)).toBe("110010");
+        expect(decToBin(150)).toBe("10010110");
+    }
+);
