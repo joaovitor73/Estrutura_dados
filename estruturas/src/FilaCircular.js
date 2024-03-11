6ef181485e9a26c8ac72d363a899d64714aa752b
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2024-03-11 06:42:43 -0300

    add fila circular

--- estruturas/src/FilaCircular.js
+++ estruturas/src/FilaCircular.js
@@ -5,20 +5,64 @@ class FilaCircular {
         this.fim = 0;
         this.dados = [];
     }
-    enqueue(dado) { }
-    dequeue() { }
-    front() { }
+    enqueue(dado) { 
+        if(!this.isFull()){
+            this.dados[this.fim] = dado;
+            if(this.fim ===  this.capacidade-1){
+                this.fim = 0;
+            }else{
+                this.fim++;
+            }
+            return;
+        }
+        throw new Error("Queueoverflow");
+    }
+    dequeue() {
+        if(!this.isFull()){
+            if(this.inicio ===  this.capacidade-1){
+                this.inicio = 0;
+            }else{
+                this.inicio++;
+            }
+            return;
+        }
+        throw new Error("Queueunderflow");
+     }
+
+    front() { 
+        if(!this.isFull()){
+            return this.dados[this.inicio];
+        }
+        throw new Error("queue empty");
+    }
     isEmpty() {
         return this.inicio === this.fim;
     }
     isFull() {
-        return this.length() === this.capacidade;
+        return (this.length() === this.capacidade) || ( this.fim == this.inicio+1);
     }
     clear() { 
         this.inicio = 0;
         this.fim = 0;
     }
-    toString() { }
+    toString() {
+        let resposta = "";
+        if(this.inicio < this.fim){
+            for(let i = this.inicio; i <= this.fim; i++){
+                resposta+=this.dados[i];
+            }
+        }else{
+            for(let i = this.inicio; i < this.capacidade; i++){
+                resposta+=this.dados[i];
+            }
+            if(this.fim!=0){
+                for(let i = this.fim; i < this.inicio; i++){
+                    resposta+=this.dados[i];
+                }
+            }
+        }
+        return resposta;
+     }
     length() {
         if (this.inicio < this.fim) {
             return this.fim - this.inicio;
